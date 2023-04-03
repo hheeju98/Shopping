67684d8f4781429f8b54d00e72341b787c46397d
hheeju98 <hheeju98@naver.com>
2023-04-04 00:10:09 +0900

    쇼핑 카트 -UI 마무리

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function PriceCard({ text, price }) {
-  return <div></div>;
+  return (
+    <div>
+      <p className="bg-gray-50 p-8 mx-2 rounded-2xl text-center text-lg md:text-xl">
+        {text}
+      </p>
+      <p className="font-bold text-brand text-xl md:text-2xl">₩{price}</p>
+    </div>
+  );
 }
