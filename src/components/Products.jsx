96e4d50f5bee5372af1dc825d854ee9fd042836a
hheeju98 <hheeju98@naver.com>
2023-04-04 22:29:14 +0900

    제대로 리팩토링

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { getProducts } from "../api/firebase";
 import { useQuery } from "@tanstack/react-query";
 import ProductCard from "./ProductCard";
+import useProducts from "../hooks/useProduct";
 
 export default function Products() {
   const {
-    isLoading,
-    error,
-    data: products,
-  } = useQuery(["products"], getProducts, { staleTime: 1000 * 60 });
+    productsQuery: { isLoading, error, data: products },
+  } = useProducts();
+
   return (
     <>
       {isLoading && <p>Loading...</p>}
