ca66283be003ec3bf42ff2ca2e6c4a7aa03bc732
hheeju98 <hheeju98@naver.com>
2023-04-04 23:09:06 +0900

    리팩토링

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -13,7 +13,7 @@ export default function ProductCard({
       }}
       className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
     >
-      <img classname="w-full" src={image} alt={title} />
+      <img className="w-full" src={image} alt={title} />
       <div className="mt-2 px-2 text-lg flex justify-between items-center">
         <h3 className="truncate">{title}</h3>
         <p>{`₩${price}`}</p>
